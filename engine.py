a47d1888a609a57255037b8483da483a87903f88
Tom Rothe <info@tomrothe.de>
2021-11-21 12:13:06 +0100

    move constants to appropriate class

--- engine.py
+++ engine.py
@@ -9,12 +9,7 @@ from pygame.locals import *
 
 PIXEL_WIDTH = 14
 PIXEL_HEIGHT = 25
-PIXEL_DISPLAY_SIZE = 30
-PIXEL_DISPLAY_INSET = 1 # px on each side
-PIXEL_DISPLAY_INNER_SIZE = PIXEL_DISPLAY_SIZE - 2*PIXEL_DISPLAY_INSET
 FPS = 30
-
-BACKGROUND_COLOR = (35, 35, 35)
 
 class Strip(object):
   def __init__(self):
@@ -40,12 +35,18 @@ class Strip(object):
   def show(self):
     pass
 
+
 class VirtualStrip(Strip):
+  PIXEL_DISPLAY_SIZE = 30
+  PIXEL_DISPLAY_INSET = 1 # px on each side
+  PIXEL_DISPLAY_INNER_SIZE = PIXEL_DISPLAY_SIZE - 2*PIXEL_DISPLAY_INSET
+  BACKGROUND_COLOR = (35, 35, 35)
+
   def __init__(self):
     super(VirtualStrip, self).__init__()
     pygame.init()
     pygame.display.set_caption("Wandpixel")
-    self.surface = pygame.display.set_mode((PIXEL_WIDTH*PIXEL_DISPLAY_SIZE, PIXEL_HEIGHT*PIXEL_DISPLAY_SIZE))
+    self.surface = pygame.display.set_mode((PIXEL_WIDTH*self.PIXEL_DISPLAY_SIZE, PIXEL_HEIGHT*self.PIXEL_DISPLAY_SIZE))
     self.pixel_colors = [ [(0, 0, 0)]*PIXEL_WIDTH for _ in range(PIXEL_HEIGHT)]
     
   def do_loop(self):
@@ -65,12 +66,13 @@ class VirtualStrip(Strip):
     self.pixel_colors[coordinate_or_index[1]][coordinate_or_index[0]] = color
 
   def show(self):
-    self.surface.fill(BACKGROUND_COLOR)
+    self.surface.fill(self.BACKGROUND_COLOR)
     for y in range(PIXEL_HEIGHT):
       for x in range(PIXEL_WIDTH):
-        left, top = x*PIXEL_DISPLAY_SIZE, y*PIXEL_DISPLAY_SIZE
-        self.surface.fill(self.pixel_colors[y][x], rect=(left+PIXEL_DISPLAY_INSET, top+PIXEL_DISPLAY_INSET, PIXEL_DISPLAY_INNER_SIZE, PIXEL_DISPLAY_INNER_SIZE))
+        left, top = x*self.PIXEL_DISPLAY_SIZE, y*self.PIXEL_DISPLAY_SIZE
+        self.surface.fill(self.pixel_colors[y][x], rect=(left+self.PIXEL_DISPLAY_INSET, top+self.PIXEL_DISPLAY_INSET, self.PIXEL_DISPLAY_INNER_SIZE, self.PIXEL_DISPLAY_INNER_SIZE))
     pygame.display.flip()
+
 
 class LedStrip(Strip):
   def __init__(self):
