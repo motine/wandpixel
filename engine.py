78ca472d4f9d5cbfb40feb26e385e18c093d7500
Tom Rothe <info@tomrothe.de>
2021-11-21 11:47:39 +0100

    add base class

--- engine.py
+++ engine.py
@@ -16,42 +16,58 @@ FPS = 30
 
 BACKGROUND_COLOR = (35, 35, 35)
 
-class Strip:
+class Strip(object):
   def __init__(self):
-    self.pixel_colors = [ [(0, 0, 0)]*PIXEL_WIDTH for i in range(PIXEL_HEIGHT)]
+    pass
+
+  def set_pixel(self, coordinate_or_index, color):
+    pass
+
+  def show(self):
+    pass
+
+class VirtualStrip(Strip):
+  def __init__(self):
+    super(VirtualStrip, self).__init__()
+    pygame.init()
+    pygame.display.set_caption("Wandpixel")
+    self.surface = pygame.display.set_mode((PIXEL_WIDTH*PIXEL_DISPLAY_SIZE, PIXEL_HEIGHT*PIXEL_DISPLAY_SIZE))
+    self.pixel_colors = [ [(0, 0, 0)]*PIXEL_WIDTH for _ in range(PIXEL_HEIGHT)]
+    
+  def shouldQuit(self):
+    for event in pygame.event.get():
+      if (event.type == pygame.QUIT):
+        return True
+    return False
 
   def set_pixel(self, coordinate_or_index, color):
     # this would use for the real pixel
     self.pixel_colors[coordinate_or_index[1]][coordinate_or_index[0]] = color
 
   def show(self):
-    surface.fill(BACKGROUND_COLOR)
+    self.surface.fill(BACKGROUND_COLOR)
     for y in range(PIXEL_HEIGHT):
       for x in range(PIXEL_WIDTH):
         left, top = x*PIXEL_DISPLAY_SIZE, y*PIXEL_DISPLAY_SIZE
-        surface.fill(self.pixel_colors[y][x], rect=(left+PIXEL_DISPLAY_INSET, top+PIXEL_DISPLAY_INSET, PIXEL_DISPLAY_INNER_SIZE, PIXEL_DISPLAY_INNER_SIZE))
+        self.surface.fill(self.pixel_colors[y][x], rect=(left+PIXEL_DISPLAY_INSET, top+PIXEL_DISPLAY_INSET, PIXEL_DISPLAY_INNER_SIZE, PIXEL_DISPLAY_INNER_SIZE))
     pygame.display.flip()
 
 if __name__ == "__main__":
   if len(sys.argv) <= 1:
     print('please specify a script')
     sys.exit(1)
+
   draw_module = importlib.import_module(sys.argv[1])
   if hasattr(draw_module, 'FPS'):
     FPS = draw_module.FPS
 
-  pygame.init() 
+  strip = VirtualStrip()
   fps_clock = pygame.time.Clock()
-  surface = pygame.display.set_mode((PIXEL_WIDTH*PIXEL_DISPLAY_SIZE, PIXEL_HEIGHT*PIXEL_DISPLAY_SIZE))
-  pygame.display.set_caption("Wandpixel")
-
-  strip = Strip()
 
   gameLoop = True
   while gameLoop:
-    for event in pygame.event.get():
-      if (event.type == pygame.QUIT):
-        gameLoop = False
+    if strip.shouldQuit():
+      gameLoop = False
     draw_module.draw(strip)
     fps_clock.tick(FPS)
   pygame.quit()
