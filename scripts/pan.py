415721f70e31a47afa2487ab473e98e424b9edd1
Tom Rothe <info@tomrothe.de>
2021-11-28 12:04:39 +0100

    improve panning

--- scripts/pan.py
+++ scripts/pan.py
@@ -6,9 +6,12 @@ from lib.engine import PIXEL_WIDTH, PIXEL_HEIGHT
 from PIL import Image
 import random
 
-FPS = 5
-DIRECTION_RAND = 0.1
-DIRECTION_MAX = 1.0
+FPS = 10
+DIRECTION_RAND = 0.05
+DIRECTION_MAX = 0.5
+WINDOW_FACTOR = 5 # size of the window we crop out of the image before we size it down
+WINDOW_WIDTH = PIXEL_WIDTH * WINDOW_FACTOR
+WINDOW_HEIGHT = PIXEL_HEIGHT * WINDOW_FACTOR
 image = None
 offset = [0.0, 0.0]
 direction = [1.0, 0.1]
@@ -27,7 +30,8 @@ def draw(strip):
   
   current_image = image.copy()
   offset_x, offset_y = int(offset[0]), int(offset[1])
-  current_image = current_image.crop((offset_x, offset_y, PIXEL_WIDTH+offset_x, PIXEL_HEIGHT+offset_y))
+  current_image = current_image.crop((offset_x, offset_y, offset_x+WINDOW_WIDTH, offset_y+WINDOW_HEIGHT))
+  current_image.thumbnail((PIXEL_WIDTH, PIXEL_HEIGHT))
 
   # draw
   strip.fill((0, 0, 0))
@@ -35,8 +39,8 @@ def draw(strip):
     x, y = int(i % current_image.width), int(i / current_image.width)
     strip.set_pixel((x, y), (r, g, b))
 
-  offset[0] = (offset[0] + direction[0]) % (image.width - PIXEL_WIDTH)
-  offset[1] = (offset[1] + direction[1]) % (image.height - PIXEL_HEIGHT)
+  offset[0] = (offset[0] + direction[0]) % (image.width - WINDOW_WIDTH)
+  offset[1] = (offset[1] + direction[1]) % (image.height - WINDOW_HEIGHT)
 
   direction[0] += random.uniform(-DIRECTION_RAND, DIRECTION_RAND) # randomize
   direction[1] += random.uniform(-DIRECTION_RAND, DIRECTION_RAND)
