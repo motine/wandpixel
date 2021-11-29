a5c03dadec0e59126491e8c25d207f6322481189
Dario Litz <dario.litz@outlook.com>
2021-11-29 19:10:04 +0100

    refactors game of life

--- scripts/game_of_life.py
+++ scripts/game_of_life.py
@@ -5,17 +5,11 @@ FPS = 10
 
 def update(cur):
     nxt = np.zeros((cur.shape[0], cur.shape[1]))
-
     for r, c in np.ndindex(cur.shape):
         num_alive = np.sum(cur[r-1:r+2, c-1:c+2]) - cur[r, c]
-
         if (cur[r, c] == 1 and 2 <= num_alive <= 3) or (cur[r, c] == 0 and num_alive == 3):
             nxt[r, c] = 1
-
     return nxt
-
-old_grid = np.random.randint(0,2,(PIXEL_WIDTH,PIXEL_HEIGHT), dtype='int');
-new_grid = old_grid
 
 def generate():
   global old_grid, new_grid
@@ -28,7 +22,6 @@ def draw_grid(strip, grd):
             color = (255, 255, 255)
         else:
             color = (0, 0, 0)
-
         strip.set_pixel((iy, ix), color)
 
 def restart():
@@ -36,13 +29,12 @@ def restart():
 
 def draw(strip):
     global old_grid, new_grid
-
     strip.fill((0, 0, 0))
-
     new_grid = update(old_grid)
     draw_grid(strip, new_grid)
     if np.array_equal(old_grid, new_grid):
         restart()
     old_grid = new_grid
-    
     strip.show()
+
+generate()
